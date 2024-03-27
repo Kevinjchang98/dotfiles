89d8681dcbb31d9b0267dee87b6ab24440c6d9cc
Kevin Chang <kevinjchang98@gmail.com>
2024-03-27 00:57:15 -0700

    Add lvim config

---
+++ .config/lvim/config.lua
@@ -0,0 +1,69 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+vim.opt.relativenumber = true -- relative line numbers
+
+lvim.colorscheme = "tokyonight"
+
+-- use treesitter folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldenable = false
+
+lvim.plugins = {
+  "mfussenegger/nvim-dap",
+  "mfussenegger/nvim-dap-python",
+  "nvim-neotest/neotest",
+  "nvim-neotest/neotest-python",
+  "nvim-neotest/nvim-nio",
+  "folke/tokyonight.nvim",
+}
+
+lvim.builtin.dap.active = true
+local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+
+-- Python dap
+require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
+
+require("neotest").setup({
+  adapters = {
+    require("neotest-python")({
+      dap = {
+        justMyCode = false,
+        console = "integratedTerminal",
+      },
+      args = { "--log-level", "DEBUG", "--quiet" },
+      runner = "pytest",
+    })
+  }
+})
+
+
+-- Rust dap
+local dap = require("dap")
+dap.adapters.codelldb = {
+  type = "server",
+  port = "${port}",
+  executable = {
+    command = mason_path .. "packages/codelldb/extension/adapter/codelldb",
+    args = { "--port", "${port}" },
+  }
+}
+
+dap.configurations.cpp = {
+  {
+    name = "Launch",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    end,
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
+  },
+}
+
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
